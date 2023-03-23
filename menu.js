974a8c97be12edfce2331cd5beb09904e281f74c
Matheus Lima <masblima@rd.com.br>
2023-03-23 12:55:24 -0300

    first commit

---
+++ menu.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+const child_process = require('child_process');
+
+inquirer
+  .prompt([
+    {
+      type: 'rawlist',
+      name: 'subject',
+      message: 'What subject?',
+      choices: ['Sort', 'Tree'],
+      filter: function(val) {
+        return val.toLowerCase();
+      }
+    },
+    {
+      type: 'rawlist',
+      name: 'algorithm',
+      pageSize: 15,
+      message: 'What Algorithm?',
+      choices: ['Bubble Sort', 'Insertion Sort'],
+      filter: function(val) {
+        return val.toLowerCase().split(' ').join('-');
+      }
+    }
+  ])
+  .then((answers) => {
+    const spawn = child_process.spawn('yarn', [`${answers.subject}:${answers.algorithm}`], {stdio: "inherit"});
+
+    spawn.stdout.on('data', function(data){
+      child_process.stdout.write(data);
+    });
+    
+    spawn.stderr.on('data', function(data){
+      child_process.stderr.write(data);
+    });
+    
+    spawn.on('exit', function(code){
+      console.log(code);
+    });
+    
+  });
