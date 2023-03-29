effc688c4a7313e0e8de03f49d687281072ec685
Matheus Lima <masblima@rd.com.br>
2023-03-29 11:36:21 -0300

    add version 3 bubble-sort

---
+++ src/sort/bubble-sort/bubble-sort-v3.ts
@@ -0,0 +1,22 @@
+export class BubbleSortV3 {
+    sort(data: number[]): number[] {
+        const size: number = data.length;
+        let aux: number = 0;
+        let swap: boolean = true;
+        let n = 1;
+
+        while (n <= size && swap) {
+            for (let index = 0; index < size - 1; index++) {
+                if(data[index] > data[index + 1]) {
+                    swap = true;
+                    aux = data[index];
+                    data[index] = data[index + 1];
+                    data[index + 1] = aux;
+                }
+            }
+            n++
+        }
+
+        return data;
+    }
+}
--- src/sort/bubble-sort/bubble-sort.test.ts
+++ src/sort/bubble-sort/bubble-sort.test.ts
@@ -1,5 +1,6 @@
 import { BubbleSortV1 } from "./bubble-sort-v1";
 import { BubbleSortV2 } from "./bubble-sort-v2";
+import { BubbleSortV3 } from "./bubble-sort-v3";
 
 const emptyArray: number[] = [];
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
@@ -27,6 +28,20 @@ describe('BubbleSort Version 1', () => {
 
 describe('BubbleSort Version 2', () => {
     const bubbleSort = new BubbleSortV2()
+
+    it('should sort array', () => {
+        expect(bubbleSort.sort(emptyArray)).toEqual(emptyArray);
+        expect(bubbleSort.sort(sortedArray)).toEqual(sortedArray);
+        expect(bubbleSort.sort(reverseArray)).toEqual(sortedArray);
+        expect(bubbleSort.sort(notSortedArray)).toEqual(sortedArray);
+        expect(bubbleSort.sort(equalArray)).toEqual(equalArray);
+        expect(bubbleSort.sort(negativeArray)).toEqual(negativeArraySorted);
+        expect(bubbleSort.sort(negativeArraySorted)).toEqual(negativeArraySorted);
+    });
+});
+
+describe('BubbleSort Version 3', () => {
+    const bubbleSort = new BubbleSortV3()
 
     it('should sort array', () => {
         expect(bubbleSort.sort(emptyArray)).toEqual(emptyArray);
